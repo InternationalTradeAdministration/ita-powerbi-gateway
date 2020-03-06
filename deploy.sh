b20526b15fe3adb827708c1be3a37390dddaf7fe
lipsotiko <lipsotiko@gmail.com>
2020-03-06 08:33:54 -0500

    Updated docker registry in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #You'll need to log into Azure first 'sudo az acr login'
-sudo az acr login --name itadataloader
-sudo docker login itadataloader.azurecr.io -u itadataloader -p $AZURE_CONTAINER_KEY  #use container username and access key
-sudo docker build -t itadataloader.azurecr.io/ita-dataloader-reporting .
-sudo docker push itadataloader.azurecr.io/ita-dataloader-reporting
+sudo az acr login --name dataservices
+sudo docker login dataservices.azurecr.io -u dataservices -p $AZURE_CONTAINER_KEY  #use container username and access key
+sudo docker build -t dataservices.azurecr.io/ita-dataloader-reporting .
+sudo docker push dataservices.azurecr.io/ita-dataloader-reporting

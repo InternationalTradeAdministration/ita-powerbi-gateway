78ea603a201c3a34c22a7b201e60498fbea48f81
lipsotiko <lipsotiko@gmail.com>
2020-03-12 09:15:03 -0400

    Removed unused dependencies, updated ci/cd pipeline config

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 sudo az acr login --name dataservices
 sudo docker login dataservices.azurecr.io -u dataservices -p $AZURE_CONTAINER_KEY  #use container username and access key
 sudo docker build -t dataservices.azurecr.io/ita-dataloader-reporting .
-sudo docker push dataservices.azurecr.io/ita-dataloader-reporting
+sudo docker push dataservices.azurecr.io/ita-dataloader-reporting:latest

bb39d2e43f07e7eba596a038dd247c724d081e81
lipsotiko <lipsotiko@gmail.com>
2020-03-25 09:38:17 -0400

    Renamed project

--- build-push-docker-image.sh
+++ build-push-docker-image.sh
@@ -3,5 +3,5 @@
 #You'll need to log into Azure first 'sudo az acr login'
 sudo az acr login --name dataservices
 sudo docker login dataservices.azurecr.io -u dataservices -p $AZURE_CONTAINER_KEY  #use container username and access key
-sudo docker build -t dataservices.azurecr.io/ita-dataloader-reporting .
-sudo docker push dataservices.azurecr.io/ita-dataloader-reporting:latest
+sudo docker build -t dataservices.azurecr.io/ita-powerbi-gateway .
+sudo docker push dataservices.azurecr.io/ita-powerbi-gateway:latest
--- deploy-aks.sh
+++ deploy-aks.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 ./build-push-docker-image.sh
-kubectl delete deployment,service ita-dataloader-reporting -n power-bi-gateway
-kubectl delete ingress ita-dataloader-reporting -n power-bi-gateway
-
-kubectl apply -f kube-config.yml
+kubectl delete deployment,service,ingress ita-powerbi-gateway -n mdsnamespace
+kubectl apply -f kube-config.yml -n mdsnamespace

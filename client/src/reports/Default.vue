bd630ff036497afb9c3154e68cedd74f666d79a4
lipsotiko <lipsotiko@gmail.com>
2020-06-04 10:48:40 -0400

    Added a fullscreen button to experiment with Drupal

--- client/src/reports/Default.vue
+++ client/src/reports/Default.vue
@@ -2,36 +2,44 @@
   <div>
     <span v-if="loading">loading...</span>
     <div id="embed-container" ref="embed-container"></div>
+    <button @click="fullscreen"><i class="material-icons">&#xe5d0;</i></button>
   </div>
 </template>
 <script>
 export default {
-  name: "Default",
+  name: 'Default',
   props: ['repository', 'pbi'],
   data: () => ({
     loading: true
   }),
-  async created() {
+  async created () {
     let r = await this.repository.generateToken(
       this.$route.params.workspaceName,
       this.$route.params.reportName
-    );
+    )
 
-    this.loading = false;
+    this.loading = false
 
     var embedConfig = {
       id: r.powerBiReport.id,
       embedUrl: r.powerBiReport.embedUrl,
       accessToken: r.powerBiToken.token,
-      type: "report",
+      type: 'report',
       tokenType: this.pbi.models.TokenType.Embed,
       permissions: this.pbi.models.Permissions.All
-    };
+    }
 
-    let embedContainer = this.$refs["embed-container"];
-    window.powerbi.embed(embedContainer, embedConfig);
+    let embedContainer = this.$refs['embed-container']
+    window.powerbi.embed(embedContainer, embedConfig)
+  },
+  methods: {
+    fullscreen () {
+      let embedContainer = this.$refs['embed-container']
+      const report = window.powerbi.get(embedContainer)
+      report.fullscreen()
+    }
   }
-};
+}
 </script>
 <style scoped>
 #embed-container {

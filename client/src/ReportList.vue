5dcd498c04cfa2c9baad76f4d2ebda6e4089a551
lipsotiko <lipsotiko@gmail.com>
2020-06-09 19:24:28 -0400

    [9374] Added toolbar for fullscreen and export data buttons

--- client/src/ReportList.vue
+++ client/src/ReportList.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="container">
-    <h1>{{workspaceName}}</h1>
+    <header>
+      <h1>{{ workspaceName }}</h1>
+    </header>
     <span v-if="loading">loading...</span>
     <div v-else>
       <table>
+        <th>
+          Name
+        </th>
         <tr v-for="r in reports" :key="r.id">
           <td>
-            <router-link :to="buildReportUrl(r.name)">{{r.name}}</router-link>
+            <router-link :to="buildReportUrl(r.name)">{{ r.name }}</router-link>
           </td>
         </tr>
       </table>
@@ -15,23 +20,23 @@
 </template>
 <script>
 export default {
-  name: "ReportList",
+  name: 'ReportList',
   props: ['repository'],
   data: () => ({
     loading: true,
     workspaceName: null,
     reports: []
   }),
-  async created() {
-    this.workspaceName = this.$route.params.workspaceName;
-    let reports = await this.repository.listReports(this.workspaceName);
-    this.reports = reports.sort((a, b) => (a.name > b.name ? 1 : -1));
-    this.loading = false;
+  async created () {
+    this.workspaceName = this.$route.params.workspaceName
+    let reports = await this.repository.listReports(this.workspaceName)
+    this.reports = reports.sort((a, b) => (a.name > b.name ? 1 : -1))
+    this.loading = false
   },
   methods: {
-    buildReportUrl(reportName) {
-      return "/workspace/" + this.workspaceName + "/report/" + reportName;
+    buildReportUrl (reportName) {
+      return '/workspace/' + this.workspaceName + '/report/' + reportName
     }
   }
-};
+}
 </script>

eff12e288d7f54c7877eccbe4ea919c853295691
lipsotiko <lipsotiko@gmail.com>
2020-06-12 13:01:36 -0400

    [9454] OTEXA users have conditionally rendered filters to choose from - Toolbar renders child components (buttons) on the left side - Toolbar is inclusive when displaying export buttons for visual types - OTEXA Annual reports can conditionally render fields using a query param

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,23 +8,5 @@ export default {
 }
 </script>
 <style>
-html {
-  font-family: Arial, Verdana, sans-serif;
-}
-
-body {
-  margin: 0;
-}
-
-h1 {
-  font-size: 28px;
-}
-
-.container {
-  margin: 0 22px;
-}
-
-iframe {
-  border: none;
-}
+  @import './styles/general.css';
 </style>
--- client/src/reports/OtexaAnnual.vue
+++ client/src/reports/OtexaAnnual.vue
@@ -1,76 +1,114 @@
 <template>
-  <div>
+  <div class="content">
     <span v-if="loading">loading...</span>
     <div v-else-if="!isReportVisible">
       <div class="filter-pane">
-        <div class="filter-field">
-          <label for="countries">Countries</label>
-          <select v-model="selectedCountries" name="countries" id="countries" multiple>
-            <option
-              v-for="item in countries"
-              :key="item.ctryId"
-              :value="item.ctryId"
-            >{{item.ctryDescription}}</option>
-          </select>
+        <div class="filter-fields">
+          <div class="filter-field">
+            <label>Data By:</label>
+            <select v-model="onlyCountry" @click="reset()">
+              <option :value="true">Country</option>
+              <option :value="false">Category</option>
+            </select>
+          </div>
+          <div class="filter-field" v-if="onlyCountry">
+            <label for="countries">Countries:</label>
+            <select
+              v-model="selectedCountries"
+              name="countries"
+              id="countries"
+              multiple
+              size="20"
+            >
+              <option
+                v-for="item in countries"
+                :key="item.ctryId"
+                :value="item.ctryId"
+                >{{ item.ctryDescription }}</option
+              >
+            </select>
+          </div>
+          <div class="filter-field" v-if="!onlyCountry">
+            <label for="categories">Categories:</label>
+            <select
+              v-model="selectedCategories"
+              name="categories"
+              id="categories"
+              multiple
+              size="20"
+              @change="updateHts()"
+            >
+              <option
+                v-for="item in categories"
+                :key="item.catId"
+                :value="item.catId"
+                >{{ item.longCategory }}</option
+              >
+            </select>
+          </div>
+          <div class="filter-field" v-if="!onlyCountry">
+            <label for="chapters">Chapters:</label>
+            <select
+              v-model="selectedChapters"
+              name="chapters"
+              id="chapters"
+              multiple
+              size="20"
+              @change="updateHts()"
+            >
+              <option
+                v-for="item in chapters"
+                :key="item.chapter"
+                :value="item.chapter"
+                >{{ item.longChapter }}</option
+              >
+            </select>
+          </div>
+          <div class="filter-field" v-if="!onlyCountry">
+            <label for="hts">HTS:</label>
+            <span v-if="loadingHts">loading...</span>
+            <select
+              v-else
+              v-model="selectedHts"
+              name="hts"
+              id="hts"
+              multiple
+              size="20"
+            >
+              <option v-for="item in hts" :key="item.hts" :value="item.hts">{{
+                item.longHts
+              }}</option>
+            </select>
+          </div>
         </div>
-        <div class="filter-field">
-          <label for="categories">Categories</label>
-          <select
-            v-model="selectedCategories"
-            name="categories"
-            id="categories"
-            multiple
-            @change="updateHts()"
-          >
-            <option
-              v-for="item in categories"
-              :key="item.catId"
-              :value="item.catId"
-            >{{item.longCategory}}</option>
-          </select>
-        </div>
-        <div class="filter-field">
-          <label for="chapters">Chapters</label>
-          <select
-            v-model="selectedChapters"
-            name="chapters"
-            id="chapters"
-            multiple
-            @change="updateHts()"
-          >
-            <option
-              v-for="item in chapters"
-              :key="item.chapter"
-              :value="item.chapter"
-            >{{item.longChapter}}</option>
-          </select>
-        </div>
-        <div class="filter-field">
-          <label for="hts">HTS</label>
-          <span v-if="loadingHts">loading...</span>
-          <select v-else v-model="selectedHts" name="hts" id="hts" multiple>
-            <option v-for="item in hts" :key="item.hts" :value="item.hts">{{item.longHts}}</option>
-          </select>
-        </div>
+        <p v-if="!onlyCountry">
+          *Multiple selections will be added together (use the Shift key for
+          sequential selections and the Ctrl key for non-sequential ones).
+        </p>
         <div class="filter-buttons">
           <button @click="viewReport()" id="submit-button">Submit</button>
           <button @click="reset()" id="reset-button">Reset</button>
         </div>
       </div>
-      <p>*Multiple selections will be added together (use the Shift key for sequential selections and the Ctrl key for non-sequential ones).</p>
     </div>
     <div :class="getReportClass()">
-      <button class="back-button" @click="back()">
-        <img src="/images/back-arrow.svg" />
-      </button>
+      <toolbar :loadingReport="loadingReport" :pbi="pbi">
+        <button @click="isReportVisible = false">
+          <img src="/images/back-arrow.svg" />
+        </button>
+      </toolbar>
       <div id="embed-container" ref="embed-container"></div>
     </div>
   </div>
 </template>
 <script>
+import Toolbar from '@/Toolbar'
 export default {
-  name: "OtexaAnnual",
+  name: 'OtexaAnnual',
   props: ['repository', 'pbi'],
+  components: {
+    toolbar: Toolbar
+  },
   data: () => ({
     report: null,
     countries: [],
@@ -83,156 +121,188 @@ export default {
     selectedHts: [],
     isReportVisible: false,
     loading: true,
-    loadingHts: false
+    loadingReport: true,
+    loadingHts: false,
+    onlyCountry: null
   }),
-  async created() {
-    this.countries = await this.repository.getOtexaCountries();
-    this.categories = await this.repository.getOtexaCategories();
-    this.chapters = await this.repository.getOtexaChapters();
+  async created () {
+    this.onlyCountry = this.$route.query.onlyCountry
+      ? this.$route.query.onlyCountry === 'true'
+      : true
+
+    this.countries = await this.repository.getOtexaCountries()
+    this.categories = await this.repository.getOtexaCategories()
+    this.chapters = await this.repository.getOtexaChapters()
 
     this.report = await this.repository.generateToken(
       this.$route.params.workspaceName,
       this.$route.params.reportName
-    );
+    )
 
-    this.loading = false;
+    this.loading = false
   },
   methods: {
-    getReportClass() {
-      return this.isReportVisible ? "visible" : "hidden";
+    getReportClass () {
+      return this.isReportVisible ? 'sub-content visible' : 'hidden'
     },
-    getHtsClass() {
-      return this.hts.length === 0 ? "not-rendered" : "";
-    },
-    async updateHts() {
+    async updateHts () {
       if (!this.htsDisabled()) {
-        this.loadingHts = true;
+        this.loadingHts = true
         this.hts = await this.repository.getOtexaHts(
           this.selectedCategories,
           this.selectedChapters
-        );
-        this.loadingHts = false;
+        )
+        this.loadingHts = false
       }
     },
-    htsDisabled() {
-      return !(this.selectedCategories.length > 0 || this.selectedChapters.length > 0);
+    htsDisabled () {
+      return !(
+        this.selectedCategories.length > 0 || this.selectedChapters.length > 0
+      )
     },
-    viewReport() {
-      let filters = [];
+    async viewReport () {
+      let filters = []
+
+      filters.push(this.filter('Display In', 'In', ['DOLLARS'], true))
 
       if (this.selectedCountries.length > 0) {
         let selectedCountries = this.countries
           .filter(c => this.selectedCountries.includes(c.ctryId))
-          .map(c => c.ctryDescription);
-        filters.push(this.filter("Country", selectedCountries));
+          .map(c => c.ctryDescription)
+        filters.push(this.filter('Country', 'In', selectedCountries, false))
+      } else {
+        filters.push(this.filter('Country', 'All', [], false))
       }
 
       if (this.selectedCategories.length > 0) {
         let selectedCategories = this.categories
           .filter(c => this.selectedCategories.includes(c.catId))
-          .map(c => c.longCategory);
-        filters.push(this.filter("Category", selectedCategories));
+          .map(c => c.longCategory)
+        filters.push(this.filter('Category', 'In', selectedCategories, false))
+      } else {
+        filters.push(this.filter('Category', 'All', [], false))
       }
 
       if (this.selectedChapters.length > 0) {
         let selectedChapters = this.chapters
           .filter(c => this.selectedChapters.includes(c.chapter))
-          .map(c => c.longChapter);
-        filters.push(this.filter("Chapter", selectedChapters));
+          .map(c => c.longChapter)
+        filters.push(this.filter('Chapter', 'In', selectedChapters, false))
+      } else {
+        filters.push(this.filter('Chapter', 'All', [], false))
       }
 
       if (this.selectedHts.length > 0) {
         let selectedHts = this.hts
           .filter(c => this.selectedHts.includes(c.hts))
-          .map(c => c.longHts);
-        filters.push(this.filter("HTS", selectedHts));
+          .map(c => c.longHts)
+        filters.push(this.filter('HTS', 'In', selectedHts, false))
+      } else {
+        filters.push(this.filter('HTS', 'All', [], false))
       }
 
       var embedConfig = {
         id: this.report.powerBiReport.id,
         embedUrl: this.report.powerBiReport.embedUrl,
         accessToken: this.report.powerBiToken.token,
-        type: "report",
+        type: 'report',
         tokenType: this.pbi.models.TokenType.Embed,
         permissions: this.pbi.models.Permissions.All,
         settings: {
           filterPaneEnabled: true
-        },
-        filters
-      };
+        }
+      }
 
-      let embedContainer = this.$refs["embed-container"];
-      
-      window.powerbi.embed(embedContainer, embedConfig);
+      let embedContainer = this.$refs['embed-container']
 
-      this.isReportVisible = true;
+      window.powerbi.embed(embedContainer, embedConfig)
+
+      const report = window.powerbi.get(embedContainer)
+      report.on('loaded', async () => {
+        this.loadingReport = false
+
+        report.setFilters(filters)
+
+        if (!this.onlyCountry) {
+          let pages = await report.getPages()
+          pages.filter(p => p.displayName === 'Country')[0].setActive()
+        }
+      })
+
+      this.isReportVisible = true
     },
-    reset() {
-      this.selectedCountries = [];
-      this.selectedCategories = [];
-      this.selectedChapters = [];
-      this.selectedHts = [];
-      this.hts = [];
-      this.isReportVisible = false;
+    reset () {
+      this.selectedCountries = []
+      this.selectedCategories = []
+      this.selectedChapters = []
+      this.selectedHts = []
+      this.hts = []
+      this.isReportVisible = false
     },
-    filter(column, values) {
+    filter (column, operator, values, requireSingleSelection) {
       return {
-        $schema: "http://powerbi.com/product/schema#basic",
+        requireSingleSelection,
+        operator,
         values,
-        operator: "In",
+        $schema: 'http://powerbi.com/product/schema#basic',
         target: {
           column,
-          table: "OTEXA_EXE_HTS_VW2"
+          table: 'OTEXA_EXE_HTS_VW2'
         },
         filterType: 1
-      };
-    },
-    back() {
-      this.isReportVisible = false;
+      }
     }
   }
-};
+}
 </script>
 <style scoped>
-#embed-container {
-  height: 95vh;
-}
-
-.not-rendered {
-  display: none;
+.sub-content {
+  display: flex;
+  flex-flow: column;
+  height: 100%;
 }
 
 .visible {
   visibility: visible;
+  height: 100%;
 }
 
 .hidden {
   visibility: hidden;
 }
 
-.filter-pane {
+.filter-fields {
   display: flex;
 }
 
-.filter-field label {
-  font-weight: bold;
+input,
+label {
+  display: block;
 }
 
 .filter-field {
-  display: grid;
+  padding: 8px 12px;
+}
+
+.filter-field label {
+  margin-bottom: 8px;
 }
 
 .filter-field select {
-  height: 148px;
+  padding: 4px 8px;
+}
+
+.filter-buttons {
+  display: flex;
+  justify-content: flex-end;
+  padding: 8px;
 }
 
 .filter-buttons button {
   margin: 20px 0 0 20px;
 }
 
-.back-button {
-  border: none;
-  background: white;
-  cursor: pointer;
+p {
+  padding: 8px;
 }
 </style>

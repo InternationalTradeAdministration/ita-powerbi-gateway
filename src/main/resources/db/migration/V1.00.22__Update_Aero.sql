b97a44ad46dc4d7771e246bcd6dccfc989b9afc4
Donna Hogan <ddhogan@users.noreply.github.com>
2020-06-17 16:35:29 -0400

    [9642] Add the quarterly trade flow data

---
+++ src/main/resources/db/migration/V1.00.22__Update_Aero.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS dbo.AERO_TRADE_QUARTERLY
+GO
+
+CREATE TABLE dbo.AERO_TRADE_QUARTERLY(
+    "Trade Flow" varchar(255),
+    "Partner" varchar(255),
+    "Year" int,
+    "Q01" varchar(255),
+    "Q02" varchar(255),
+    "Q03" varchar(255),
+    "Q04" varchar(255),
+);
+GO

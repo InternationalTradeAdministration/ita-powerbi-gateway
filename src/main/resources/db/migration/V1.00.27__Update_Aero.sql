6c4474b40ef380818a2900bc5b5929abecccd08f
Donna Hogan <ddhogan@users.noreply.github.com>
2020-07-21 14:00:18 -0400

    [11116] Add Aero Data: Defense Total Exports

---
+++ src/main/resources/db/migration/V1.00.27__Update_Aero.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS dbo.AERO_DEFENSE_TOTAL_EXPORTS
+GO
+
+CREATE TABLE dbo.AERO_DEFENSE_TOTAL_EXPORTS(
+    "HS" varchar(255),
+    "PARTNER" varchar(255),
+    "Year" char(6),
+    "Value" decimal(19,0)
+)
+GO

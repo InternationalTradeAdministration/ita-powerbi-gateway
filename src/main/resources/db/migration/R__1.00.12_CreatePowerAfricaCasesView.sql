1bdc16bf550dcc5aab81e6b622a794692b61657e
Donna Hogan <ddhogan@users.noreply.github.com>
2020-08-28 12:44:41 -0400

    [12557] User sees Power Africa data in MDS
    - Add `Subject` and `Status` for `Case`s

--- src/main/resources/db/migration/R__1.00.12_CreatePowerAfricaCasesView.sql
+++ src/main/resources/db/migration/R__1.00.12_CreatePowerAfricaCasesView.sql
@@ -2,6 +2,8 @@ CREATE OR ALTER VIEW [power_africa_reader].[cases]
 AS
 SELECT id              [Case ID],
        number          [Case Number],
+       subject         [Subject],
+       status          [Status],
        related_case_id [Related Case ID],
        organization_id [Organization ID]
 FROM [power_africa].[cases]
--- src/main/resources/db/migration/V1.00.43__CreatePowerAfricaCasesTable.sql
+++ src/main/resources/db/migration/V1.00.43__CreatePowerAfricaCasesTable.sql
@@ -3,6 +3,7 @@ CREATE TABLE [power_africa].[cases]
     id              nvarchar(128) NOT NULL,
     number          nvarchar(128) NOT NULL,
     subject         nvarchar(255) NOT NULL,
+    status          nvarchar(128) NOT NULL,
     related_case_id nvarchar(128) NULL,
     organization_id nvarchar(128) NOT NULL,
     created_at      datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP

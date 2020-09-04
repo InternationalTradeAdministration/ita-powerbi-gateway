8e89f26879abe19e3f3b12fabcd1e544612933a9
azaidi12 <ahzaidi550@gmail.com>
2020-09-04 16:22:33 -0400

    [12965] Update SIMA Concordance and License_VW schemas

--- src/main/resources/db/migration/V1.00.48__UpdateSIMA.sql
+++ src/main/resources/db/migration/V1.00.48__UpdateSIMA.sql
@@ -1,3 +1,20 @@
+DROP TABLE IF EXISTS [dbo].[SIMA_STEEL_CONCORDANCE]
+GO
+
+CREATE TABLE dbo.SIMA_STEEL_CONCORDANCE
+(
+    "Number"     nvarchar(MAX),
+    "New Product Group" nvarchar(MAX),
+    "HTS Code"  BIGINT,
+    "HTS Description"   nvarchar(MAX),
+    "Old Product Group"   nvarchar(MAX),
+    "6 Steel Categories"    nvarchar(MAX)
+)
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX INDEX_SIMA_STEEL_CONCORDANCE ON dbo.SIMA_STEEL_CONCORDANCE ("HTS Code")
+
+
 DROP VIEW [dbo].[SIMA_LICENSE_VW]
 GO
 CREATE VIEW [dbo].[SIMA_LICENSE_VW]
@@ -9,9 +26,9 @@ SELECT lic.[Country of Origin] as [Country]
     , YEAR(lic.[Date of Importation]) as [SYEAR]
     , ref.[New Product Group]
     , CASE WHEN [New Product Group] like '%Stainless%' then 'S' ELSE 'C & A' END as grade2
-    , ref.[6-steel categories]
+    , ref.[6 Steel Categories]
 FROM SIMA_LICENSE lic
 JOIN SIMA_STEEL_CONCORDANCE ref
-on lic.[HTS Code] = ref.[HTS Code 10 Digit]
-GROUP BY lic.[Country of Origin], MONTH(lic.[Date of Importation]), YEAR(lic.[Date of Importation]), ref.[New Product Group], ref.[6-steel categories]
+on lic.[HTS Code] = ref.[HTS Code]
+GROUP BY lic.[Country of Origin], MONTH(lic.[Date of Importation]), YEAR(lic.[Date of Importation]), ref.[New Product Group], ref.[6 Steel Categories]
 GO

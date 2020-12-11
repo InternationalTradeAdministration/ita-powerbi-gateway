33576a15738f92c304b53782d33f065e04fe178c
edsp-david <31735841+edsp-david@users.noreply.github.com>
2020-12-11 13:52:34 -0500

    [17375] Fix discrepancies on SIMA aluminum data
    - add index and constraint

--- src/main/resources/db/migration/R__1.00.22_CreateSIMAAluminumConcordance.sql
+++ src/main/resources/db/migration/R__1.00.22_CreateSIMAAluminumConcordance.sql
@@ -3,11 +3,10 @@ GO
 
 CREATE TABLE [dbo].[SIMA_Aluminum_Concordance]
 (
-    [Product Group] VARCHAR(255),
-    [HTS Code]  VARCHAR(255),
-    [HTS Description]   NVARCHAR(MAX),
-    [Category]  VARCHAR(255),
-    [AUV-MIN]   DECIMAL(10),
-    [AUV-High]  DECIMAL(10)
+    [Product Group]   NVARCHAR(255) NOT NULL,
+    [HTS Code]        CHAR(10)      NOT NULL,
+    [HTS Description] NVARCHAR(255) NOT NULL,
+    [Category]        NVARCHAR(255) NOT NULL,
 )
-GO
+
+CREATE UNIQUE NONCLUSTERED INDEX INDEX_SIMA_ALUMINUM_CONCONDANCE_ON_HTS_CODE ON [dbo].[SIMA_Aluminum_Concordance] ([HTS Code])

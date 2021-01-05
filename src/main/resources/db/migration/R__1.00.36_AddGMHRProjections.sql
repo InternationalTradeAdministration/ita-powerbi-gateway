5324b897ae2676fd4fc9cff242e7d3c6961d791d
azaidi12 <ahzaidi550@gmail.com>
2021-01-04 21:22:44 -0500

    [17784] Add GM HR Projection Tables

---
+++ src/main/resources/db/migration/R__1.00.36_AddGMHRProjections.sql
@@ -0,0 +1,88 @@
+DROP TABLE IF EXISTS [dbo].[GMHR_DH_PROJECTIONS]
+GO
+
+CREATE TABLE [dbo].[GMHR_DH_PROJECTIONS]
+(
+    [Source]    "DH Projections",
+    [Owning Org]    VARCHAR(MAX),
+    [BOC]   INTEGER,
+    [BOC_F2]    INTEGER,
+    [BOC_L2]    INTEGER,
+    [Employee Name] VARCHAR(MAX),
+    [Duty City] VARCHAR(MAX),
+    [Duty State]    VARCHAR(MAX),
+    [Plan]  VARCHAR(MAX),
+    [Pay Plan 2]    VARCHAR(MAX),
+    [Charging Org]  VARCHAR(MAX),
+    [20.7]  BIGINT,
+    [21] INTEGER,
+    [22] INTEGER,
+    [23] INTEGER,
+    [24] INTEGER,
+    [25] INTEGER,
+    [26] INTEGER,
+    [1] INTEGER,
+    [2] INTEGER,
+    [3] INTEGER,
+    [4] INTEGER,
+    [5] INTEGER,
+    [6] INTEGER,
+    [7] INTEGER,
+    [8] INTEGER,
+    [9] INTEGER,
+    [10] INTEGER,
+    [11] INTEGER,
+    [12] INTEGER,
+    [13] INTEGER,
+    [14] INTEGER,
+    [15] INTEGER,
+    [16] INTEGER,
+    [17] INTEGER,
+    [18] INTEGER,
+    [19] INTEGER,
+    [20.4] INTEGER,
+    [Total] INTEGER
+)
+GO
+
+DROP TABLE IF EXISTS [dbo].[GMHR_LES_PROJECTIONS]
+GO
+
+CREATE TABLE [dbo].[GMHR_LES_PROJECTIONS]
+(
+    [Source]    "LES Projections",
+    [DAS/ED]    VARCHAR(MAX),
+    [Country Name]  VARCHAR(MAX),
+    [BOC]   INTEGER,
+    [Fund]  VARCHAR(MAX),
+    [Name]  VARCHAR(MAX),
+    [20]  BIGINT,
+    [21] INTEGER,
+    [22] INTEGER,
+    [23] INTEGER,
+    [24] INTEGER,
+    [25] INTEGER,
+    [26] INTEGER,
+    [1] INTEGER,
+    [2] INTEGER,
+    [3] INTEGER,
+    [4] INTEGER,
+    [5] INTEGER,
+    [6] INTEGER,
+    [7] INTEGER,
+    [8] INTEGER,
+    [9] INTEGER,
+    [10] INTEGER,
+    [11] INTEGER,
+    [12] INTEGER,
+    [13] INTEGER,
+    [14] INTEGER,
+    [15] INTEGER,
+    [16] INTEGER,
+    [17] INTEGER,
+    [18] INTEGER,
+    [19] INTEGER,
+    [20.1] INTEGER,
+    [Total] INTEGER
+)
+GO

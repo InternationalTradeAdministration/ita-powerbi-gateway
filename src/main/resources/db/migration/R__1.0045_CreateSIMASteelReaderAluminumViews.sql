0b6201d19698b50f3c547b29d13a279d9539df88
Asad Zaidi <asad.zaidi@trade.gov>
2021-01-22 13:11:21 -0500

    [19024] Add Aluminum Tables to SIMA Steel Reader

---
+++ src/main/resources/db/migration/R__1.0045_CreateSIMASteelReaderAluminumViews.sql
@@ -0,0 +1,9 @@
+CREATE OR ALTER VIEW sima_steel_reader.SIMA_ALUMINUM
+AS
+SELECT * FROM [dbo].[SIMA_ALUMINUM]
+GO
+
+CREATE OR ALTER VIEW sima_steel_reader.SIMA_Aluminum_Concordance
+AS
+SELECT * FROM [dbo].[SIMA_Aluminum_Concordance]
+GO

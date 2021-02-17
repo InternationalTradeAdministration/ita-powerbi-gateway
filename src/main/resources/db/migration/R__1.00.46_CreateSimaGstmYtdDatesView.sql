6c98ed5e4de79e5a92ed3edec07d50cffb260ce6
edsp-david <31735841+edsp-david@users.noreply.github.com>
2021-02-17 08:29:02 -0500

    [19307] GSTM user sees YTD data

---
+++ src/main/resources/db/migration/R__1.00.46_CreateSimaGstmYtdDatesView.sql
@@ -0,0 +1,10 @@
+CREATE OR ALTER VIEW [sima_gstm].[YTD_DATES]
+AS
+SELECT p.[direction]
+     , p.[reporter]
+     , MAX(p.[date])                     AS [max_current_ytd_date]
+     , DATEADD(year, -1, MAX(p.[date]))  AS [max_previous_ytd_date]
+     , CAST(MIN(p.[created_at]) AS date) AS [last_updated_on]
+FROM [sima_gstm].[PRODUCTS] p
+WHERE p.[attribute] = 'Quantity'
+GROUP BY p.[direction], p.[reporter]
---
+++ src/main/resources/db/migration/R__1.00.47__CreateSimaAggregatedProductCategoriesView.sql
@@ -0,0 +1,113 @@
+CREATE OR ALTER VIEW [sima_gstm].[AGGREGATED_PRODUCTS]
+AS
+SELECT p.[direction]                   AS [Direction],
+       p.[date]                        AS [Date],
+       CAST(YEAR(p.[date]) AS CHAR(4)) AS [Date Label],
+       p.[reporter]                    AS [Reporting Country],
+       p.[partner]                     AS [Partner Country],
+       c.[product_category]            AS [Product Category],
+       p.[attribute]                   AS [Unit of Measure],
+       p.[attribute_unit]              AS [Measure Unit],
+       SUM(p.[attribute_value]) / 1000 AS [Measure Amount],
+       MAX(p.[created_at])             AS [Last Updated]
+FROM [sima_gstm].[PRODUCTS] p
+         LEFT JOIN [sima_gstm].[HS_CODES_PRODUCT_CATEGORIES] c
+                   ON p.[product] = c.[hs_code]
+         LEFT JOIN [sima_gstm].[YTD_DATES] y ON p.[direction] = y.[direction]
+    AND p.[reporter] = y.[reporter]
+WHERE YEAR(p.[date]) <= YEAR(y.[max_previous_ytd_date])
+  AND p.[attribute] = 'Quantity'
+GROUP BY p.[direction],
+         p.[reporter],
+         p.[date],
+         p.[partner],
+         c.[product_category],
+         p.[attribute],
+         p.[attribute_unit]
+UNION ALL
+SELECT p.[direction]                   AS [Direction],
+       p.[date]                        AS [Date],
+       CAST(YEAR(p.[date]) AS CHAR(4)) AS [Date Label],
+       p.[reporter]                    AS [Reporting Country],
+       p.[partner]                     AS [Partner Country],
+       c.[product_category]            AS [Product Category],
+       p.[attribute]                   AS [Unit of Measure],
+       p.[attribute_unit]              AS [Measure Unit],
+       SUM(p.[attribute_value])        AS [Measure Amount],
+       MAX(p.[created_at])             AS [Last Updated]
+FROM [sima_gstm].[PRODUCTS] p
+         LEFT JOIN [sima_gstm].[HS_CODES_PRODUCT_CATEGORIES] c
+                   ON p.[product] = c.[hs_code]
+         LEFT JOIN [sima_gstm].[YTD_DATES] y ON p.[direction] = y.[direction]
+    AND p.[reporter] = y.[reporter]
+WHERE YEAR(p.[date]) <= YEAR(y.[max_previous_ytd_date])
+  AND p.[attribute] = 'Value'
+GROUP BY p.[direction],
+         p.[reporter],
+         p.[date],
+         p.[partner],
+         c.[product_category],
+         p.[attribute],
+         p.[attribute_unit]
+UNION ALL
+SELECT p.[direction]                            AS [Direction],
+       p.[date]                                 AS [Date],
+       CONCAT(
+               'YTD ',
+               SUBSTRING(DATENAME(month, y.[max_current_ytd_date]), 1, 3),
+               ' ',
+               CAST(YEAR(p.[date]) AS CHAR(4))) AS [Date Label],
+       p.[reporter]                             AS [Reporting Country],
+       p.[partner]                              AS [Partner Country],
+       c.[product_category]                     AS [Product Category],
+       p.[attribute]                            AS [Unit of Measure],
+       p.[attribute_unit]                       AS [Measure Unit],
+       SUM(p.[attribute_value]) / 1000          AS [Measure Amount],
+       MAX(p.created_at)                        AS [Last Updated]
+FROM [sima_gstm].[PRODUCTS] p
+         LEFT JOIN [sima_gstm].[HS_CODES_PRODUCT_CATEGORIES] c
+                   ON p.[product] = c.[hs_code]
+         LEFT JOIN [sima_gstm].[YTD_DATES] y ON p.[direction] = y.[direction]
+    AND p.[reporter] = y.[reporter]
+WHERE YEAR(p.[date]) >= YEAR(y.[max_previous_ytd_date])
+  AND MONTH(p.[date]) <= MONTH(y.[max_current_ytd_date])
+  AND p.[attribute] = 'Quantity'
+GROUP BY p.[direction],
+         p.[reporter],
+         p.[date],
+         y.[max_current_ytd_date],
+         p.[partner],
+         c.[product_category],
+         p.[attribute],
+         p.[attribute_unit]
+UNION ALL
+SELECT p.[direction]                            AS [Direction],
+       p.[date]                                 AS [Date],
+       CONCAT(
+               'YTD ',
+               SUBSTRING(DATENAME(month, y.[max_current_ytd_date]), 1, 3),
+               ' ',
+               CAST(YEAR(p.[date]) AS CHAR(4))) AS [Date Label],
+       p.[reporter]                             AS [Reporting Country],
+       p.[partner]                              AS [Partner Country],
+       c.[product_category]                     AS [Product Category],
+       p.[attribute]                            AS [Unit of Measure],
+       p.[attribute_unit]                       AS [Measure Unit],
+       SUM(p.[attribute_value])                 AS [Measure Amount],
+       MAX(p.[created_at])                      AS [Last Updated]
+FROM [sima_gstm].[PRODUCTS] p
+         LEFT JOIN [sima_gstm].[HS_CODES_PRODUCT_CATEGORIES] c
+                   ON p.[product] = c.[hs_code]
+         LEFT JOIN [sima_gstm].[YTD_DATES] y ON p.[direction] = y.[direction]
+    AND p.[reporter] = y.[reporter]
+WHERE YEAR(p.[date]) >= YEAR(y.[max_previous_ytd_date])
+  AND MONTH(p.[date]) <= MONTH(y.[max_current_ytd_date])
+  AND p.[attribute] = 'Value'
+GROUP BY p.[direction],
+         p.[reporter],
+         p.[date],
+         y.[max_current_ytd_date],
+         p.[partner],
+         c.[product_category],
+         p.[attribute],
+         p.[attribute_unit]

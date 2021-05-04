5b26c00224b3ee15ab6b40de31d937c3c0ad0cfc
Donna Hogan <ddhogan@users.noreply.github.com>
2021-05-04 12:41:50 -0400

    Otexa: Remove page tabs in Merged & Part Category reports

--- client/src/reports/OtexaMergedPartCat.vue
+++ client/src/reports/OtexaMergedPartCat.vue
@@ -199,7 +199,8 @@ export default {
         tokenType: this.pbi.models.TokenType.Embed,
         permissions: this.pbi.models.Permissions.All,
         settings: {
-          filterPaneEnabled: false
+          filterPaneEnabled: false,
+          navContentPaneEnabled: false,
         }
       }
 

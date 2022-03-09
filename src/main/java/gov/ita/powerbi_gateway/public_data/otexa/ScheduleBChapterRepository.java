6a660e7088db9fba4822b8ba5f19e3dc9ff3b24b
edsp-david <31735841+edsp-david@users.noreply.github.com>
2022-03-09 17:12:42 -0500

    [29859] OTEXA users see sorted chapters in Export Data, Export Footwear Data, Export Footwear Data (Historical) reports

--- src/main/java/gov/ita/powerbi_gateway/public_data/otexa/ScheduleBChapterRepository.java
+++ src/main/java/gov/ita/powerbi_gateway/public_data/otexa/ScheduleBChapterRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 @Repository
 interface ScheduleBChapterRepository extends JpaRepository<ScheduleBChapter, Long> {
-  @Query("select distinct new gov.ita.powerbi_gateway.public_data.otexa.Chapter(t.chapter, t.longChapter) from ScheduleBChapter t where t.source=:source")
+  @Query("select distinct new gov.ita.powerbi_gateway.public_data.otexa.Chapter(t.chapter, t.longChapter) from ScheduleBChapter t where t.source=:source ORDER BY t.chapter ASC")
   List<Chapter> findBySource(String source);
 }

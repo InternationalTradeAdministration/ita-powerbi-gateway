3060c058eda2fe365f3a7a64486752ac7eae9616
edsp-david <31735841+edsp-david@users.noreply.github.com>
2022-03-18 10:33:15 -0400

    [30045] GSTM user sees annual data on the same year that Dec YTD data is available

--- src/main/resources/db/migration/R__1.00.46_CreateSimaGstmYtdDatesView.sql
+++ src/main/resources/db/migration/R__1.00.46_CreateSimaGstmYtdDatesView.sql
@@ -4,7 +4,7 @@ SELECT p.[direction]
      , p.[reporter]
      , MAX(p.[date])                     AS [max_current_ytd_date]
      , DATEADD(year, -1, MAX(p.[date]))  AS [max_previous_ytd_date]
-     , CAST(MIN(p.[created_at]) AS date) AS [last_updated_on]
+     , CAST(MAX(p.[created_at]) AS date) AS [last_updated_on]
 FROM [sima_gstm].[PRODUCTS] p
 WHERE p.[attribute] = 'Quantity'
 GROUP BY p.[direction], p.[reporter]
--- src/main/resources/db/migration/R__1.00.47__CreateSimaAggregatedProductCategoriesView.sql
+++ src/main/resources/db/migration/R__1.00.47__CreateSimaAggregatedProductCategoriesView.sql
@@ -50,11 +50,65 @@ GROUP BY p.[direction],
          p.[attribute],
          p.[attribute_unit]
 UNION ALL
+SELECT p.[direction]                                   AS [Direction],
+       p.[date]                                        AS [Date],
+       CAST(YEAR(y.[max_current_ytd_date]) AS CHAR(4)) AS [Date Label],
+       p.[reporter]                                    AS [Reporting Country],
+       p.[partner]                                     AS [Partner Country],
+       c.[product_category]                            AS [Product Category],
+       p.[attribute]                                   AS [Unit of Measure],
+       p.[attribute_unit]                              AS [Measure Unit],
+       SUM(p.[attribute_value]) / 1000                 AS [Measure Amount],
+       MAX(p.created_at)                               AS [Last Updated]
+FROM [sima_gstm].[PRODUCTS] p
+         LEFT JOIN [sima_gstm].[HS_CODES_PRODUCT_CATEGORIES] c
+                   ON p.[product] = c.[hs_code]
+         LEFT JOIN [sima_gstm].[YTD_DATES] y ON p.[direction] = y.[direction]
+    AND p.[reporter] = y.[reporter]
+WHERE MONTH(y.[max_current_ytd_date]) = 12
+  AND YEAR(p.[date]) >= YEAR(y.[max_current_ytd_date])
+  AND p.[attribute] = 'Quantity'
+GROUP BY p.[direction],
+         p.[reporter],
+         p.[date],
+         y.[max_current_ytd_date],
+         p.[partner],
+         c.[product_category],
+         p.[attribute],
+         p.[attribute_unit]
+UNION ALL
+SELECT p.[direction]                                   AS [Direction],
+       p.[date]                                        AS [Date],
+       CAST(YEAR(y.[max_current_ytd_date]) AS CHAR(4)) AS [Date Label],
+       p.[reporter]                                    AS [Reporting Country],
+       p.[partner]                                     AS [Partner Country],
+       c.[product_category]                            AS [Product Category],
+       p.[attribute]                                   AS [Unit of Measure],
+       p.[attribute_unit]                              AS [Measure Unit],
+       SUM(p.[attribute_value])                        AS [Measure Amount],
+       MAX(p.[created_at])                             AS [Last Updated]
+FROM [sima_gstm].[PRODUCTS] p
+         LEFT JOIN [sima_gstm].[HS_CODES_PRODUCT_CATEGORIES] c
+                   ON p.[product] = c.[hs_code]
+         LEFT JOIN [sima_gstm].[YTD_DATES] y ON p.[direction] = y.[direction]
+    AND p.[reporter] = y.[reporter]
+WHERE MONTH(y.[max_current_ytd_date]) = 12
+  AND YEAR(p.[date]) >= YEAR(y.[max_current_ytd_date])
+  AND p.[attribute] = 'Value'
+GROUP BY p.[direction],
+         p.[reporter],
+         p.[date],
+         y.[max_current_ytd_date],
+         p.[partner],
+         c.[product_category],
+         p.[attribute],
+         p.[attribute_unit]
+UNION ALL
 SELECT p.[direction]                            AS [Direction],
        p.[date]                                 AS [Date],
        CONCAT(
                'YTD ',
-               SUBSTRING(DATENAME(month, y.[max_current_ytd_date]), 1, 3),
+               SUBSTRING(DATENAME(MONTH, y.[max_current_ytd_date]), 1, 3),
                ' ',
                CAST(YEAR(p.[date]) AS CHAR(4))) AS [Date Label],
        p.[reporter]                             AS [Reporting Country],
@@ -85,7 +139,7 @@ SELECT p.[direction]                            AS [Direction],
        p.[date]                                 AS [Date],
        CONCAT(
                'YTD ',
-               SUBSTRING(DATENAME(month, y.[max_current_ytd_date]), 1, 3),
+               SUBSTRING(DATENAME(MONTH, y.[max_current_ytd_date]), 1, 3),
                ' ',
                CAST(YEAR(p.[date]) AS CHAR(4))) AS [Date Label],
        p.[reporter]                             AS [Reporting Country],

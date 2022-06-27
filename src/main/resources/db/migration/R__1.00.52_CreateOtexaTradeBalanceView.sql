07ed9009c59395ef37cfd898ab0131e34b13a224
pkalavala <prasanthi.kalavala@trade.gov>
2022-06-27 09:31:32 -0500

    [31174] OTEXA Users see only Years and YTD Dates of Trade Balance data in 'Year' box
    [31174] OTEXA Users see only Years and YTD Dates of Trade Balance data in 'Year' box
    
    [31174] OTEXA Users see only Years and YTD Dates of Trade Balance data in 'Year' box
    
    [31174] OTEXA Users see only Years and YTD Dates of Trade Balance data in 'Year' box

--- src/main/resources/db/migration/R__1.00.52_CreateOtexaTradeBalanceView.sql
+++ src/main/resources/db/migration/R__1.00.52_CreateOtexaTradeBalanceView.sql
@@ -15,3 +15,22 @@ LEFT OUTER JOIN [dbo].[OTEXA_COUNTRY_REF_VW] country
     ON details.[CTRYNUM] = country.[CTRY_NUMBER]
     AND country.[SOURCE] = 'ANNUAL'
 GO
+
+CREATE OR ALTER VIEW [dbo].[OTEXA_TRADE_BALANCE_YEARS_VW]
+AS
+SELECT DISTINCT [HEADER_ID]
+    , [Trade Flow]
+    , [Trade Year]
+FROM (
+    SELECT [HEADER_ID]
+        , CASE WHEN CHARINDEX('YTD',[Header_ID]) > 0
+          THEN SUBSTRING([Header_ID],11,7)
+          ELSE SUBSTRING([Header_ID],1,7)
+          END AS "Trade Flow"
+        , CASE WHEN CHARINDEX('YTD',[Header_ID]) > 0
+          THEN SUBSTRING([Header_ID],1,8)
+          ELSE SUBSTRING([Header_ID],9,4)
+          END AS "Trade Year"
+    FROM [dbo].[OTEXA_TRADE_BALANCE]
+) x
+GO

c95ecb599b34a601521f254f7f1bb2e7d60d6407
pkalavala <PrasanthiR369@gmail.com>
2023-08-29 16:44:49 -0400

    [41057] Create views in MDS database for reverse flow data for GSTM Monitor
    [41057] Create views in MDS database for reverse flow data for GSTM Monitor

--- src/main/resources/db/migration/R__1.00.46_CreateSimaGstmYtdDatesView.sql
+++ src/main/resources/db/migration/R__1.00.46_CreateSimaGstmYtdDatesView.sql
@@ -4,6 +4,7 @@ SELECT p.[direction]
      , p.[reporter]
      , MAX(p.[date])                     AS [max_current_ytd_date]
      , DATEADD(year, -1, MAX(p.[date]))  AS [max_previous_ytd_date]
+     , DATEADD(YEAR, -10, MAX(p.[date]))  AS [max_previous_10_ytd_date]
      , CAST(MAX(p.[created_at]) AS date) AS [last_updated_on]
 FROM [sima_gstm].[PRODUCTS] p
 WHERE p.[attribute] = 'Quantity'

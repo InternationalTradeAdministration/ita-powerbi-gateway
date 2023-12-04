f97610ebe16de1d7706a264f0e076b11186d3795
edsp-david <31735841+edsp-david@users.noreply.github.com>
2023-12-04 09:54:14 -0500

    IMAU team can configure IHS dataset to pull data from all reporters
    - accomodate longer hs_code/product
    
    Related story: AB#43423

---
+++ src/main/resources/db/migration/V1.00.131__LengthenHsCodeOnImauIhsDatasetHsCodesTable.sql
@@ -0,0 +1,2 @@
+ALTER TABLE [imau].[ihs_dataset_hs_codes]
+    ALTER COLUMN [hs_code] nvarchar(12) NOT NULL;
---
+++ src/main/resources/db/migration/V1.00.132__LengthenProductOnImauIhsDatasetProductsTable.sql
@@ -0,0 +1,2 @@
+ALTER TABLE [imau].[ihs_dataset_products]
+    ALTER COLUMN [product] nvarchar(12) NOT NULL;

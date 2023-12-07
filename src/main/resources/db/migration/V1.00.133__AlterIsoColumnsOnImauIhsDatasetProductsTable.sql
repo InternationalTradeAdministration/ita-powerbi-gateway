f47cb83bc21ed343f5dc68d7fe5d59ec98706f9b
edsp-david <31735841+edsp-david@users.noreply.github.com>
2023-12-07 17:27:19 -0500

    IMAU team can configure IHS dataset to pull data from all reporters
    - allow null value on reporter_iso and partner_iso
    
    Related story: AB#43423

---
+++ src/main/resources/db/migration/V1.00.133__AlterIsoColumnsOnImauIhsDatasetProductsTable.sql
@@ -0,0 +1,5 @@
+ALTER TABLE [imau].[ihs_dataset_products]
+    ALTER COLUMN [reporter_iso] nvarchar(5) NULL;
+
+ALTER TABLE [imau].[ihs_dataset_products]
+    ALTER COLUMN [partner_iso] nvarchar(5) NULL;
